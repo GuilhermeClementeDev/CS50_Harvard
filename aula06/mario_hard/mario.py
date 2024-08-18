ec1e92891d608e04463f093b028ef55de0411883
guizindev <guilhermevasco2015@gmail.com>
2024-08-18 15:21:03 -0300

    new_att

---
+++ aula06/mario_hard/mario.py
@@ -0,0 +1,29 @@
+while True:
+    try:
+        height = int(input("Height: "))
+        if height >= 0 and height <= 8:
+            break
+    except ValueError:
+        pass
+    
+spaces = height
+n = 1
+while height >= n:
+  j = 1
+  while spaces > j:
+    print (" ", end='')
+    j += 1
+  spaces -= 1  
+  len = 0
+  while len < n:
+      print("#", end='')
+      len += 1
+  
+  print(" ", end='')
+  len = 0
+  while len < n:
+      print("#", end='')
+      len += 1
+
+  print("")
+  n += 1    
